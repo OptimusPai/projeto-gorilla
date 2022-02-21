e59655c3ea007029ad4b092d98a2ce59d18f1acf
Anderson da Silva Pereira <89368551+OptimusPai@users.noreply.github.com>
2022-02-21 02:59:56 -0300

    Initial commit

---
+++ app/src/main/java/com/example/projeto_gorilla/formlogin.java
@@ -0,0 +1,14 @@
+package com.example.projeto_gorilla;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class formlogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formlogin);
+    }
+}
